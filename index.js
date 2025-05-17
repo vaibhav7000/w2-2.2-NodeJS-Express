c76518d131b12c7559857d855ab55f296dff91e7
vaibhav7000 <vchawla7000@gmail.com>
2025-05-17 17:20:45 +0530

    nodejs understanding

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 // run `node index.js` in the terminal
 
 console.log(`Hello Node.js v${process.versions.node}!`);
+
+
+// ecmascript -> website / organisation that writes the docs / specification for all the scripting lanaguages like JS. All the theoratical stuff related to JS like Number, Booleans, Promise, Strings, arrays, Objects etc will be written by the ECMA.
+// JS is the implementaion of the ECMA docs related to JS => conforms to the docs
+
+// NodeJS is the runtime / compiler (converts JS code into 0's and 1's) for the JS => whosoever is making the the compiler for the JS should implement the compiler according to the ecmascript docs for JS. It is based on the chromes V8 engine (compiler for JS on google chrome) + extra functionaly like 
+
+// => every time when the ECMASCRIPT docs for JS changes the compilers that is used to run the JS should also to be get updated according to the docs
+
+// Using NodeJS (that provides the environment to run the JS outside the browser) we can build backend applications like CLIS, Video-Player, mobile-apps, networking applications (http server)
+
+// backend-applications are processes / programs that runs on machine not on the browser
